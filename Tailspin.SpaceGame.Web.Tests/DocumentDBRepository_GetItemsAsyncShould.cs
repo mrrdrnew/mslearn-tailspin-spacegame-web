de0b50491e7c40c1e651cdba9f2a3100fe1c251b
amaury.rodriguez_contractor <amaury.rodriguez_contractor@jmfamily.com>
2020-10-20 11:18:53 -0400

    Configure code coverage tests

--- Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
+++ Tailspin.SpaceGame.Web.Tests/DocumentDBRepository_GetItemsAsyncShould.cs
@@ -52,6 +52,7 @@ namespace Tests
             Assert.That(scores, Is.All.Matches<Score>(score => score.GameRegion == gameRegion));
         }
 
+
         [TestCase(0, ExpectedResult = 0)]
         [TestCase(1, ExpectedResult = 1)]
         [TestCase(10, ExpectedResult = 10)]
